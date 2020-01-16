90fc68e5ac36e55f7eb31324637f6c7a282a6ba4
Illia Onishchenko <36299368+ilonikso@users.noreply.github.com>
2020-01-16 19:59:17 +0300

    fix

--- js/planPrice.js
+++ js/planPrice.js
@@ -1,15 +1,15 @@
 let priceLiteMonthly = '949';
-let priceLiteAnnualy = '759.20';
+let priceLiteAnnualy = '759';
 
 let pricePremiumMonthly = '1249';
-let pricePremiumAnnualy = '999.20';
+let pricePremiumAnnualy = '999';
 
 //--------------------------------------
 
 let input = document.querySelector('#plan-switch');
 let priceLite = document.querySelector('.plan__price-num--lite');
 let pricePremium = document.querySelector('.plan__price-num--premium');
-
+let discountText = document.querySelector('.plan__price-discount');
 
 
 input.addEventListener('change', function(e){
@@ -17,9 +17,13 @@ input.addEventListener('change', function(e){
     if(e.target.checked){
         priceLite.textContent = '£' + priceLiteAnnualy;
         pricePremium.textContent = '£' + pricePremiumAnnualy;
+        discountText.style.visibility = 'hidden';
+        
     } else{
         priceLite.textContent = '£' + priceLiteMonthly;
         pricePremium.textContent = '£' + pricePremiumMonthly;
+        
+        discountText.style.visibility = 'visible';
     }
 
     e.preventDefault();
